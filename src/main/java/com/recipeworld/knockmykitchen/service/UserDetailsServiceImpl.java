2a55c72b780a20986bab634124a818f200ae47e3
Sakthivel <sakthivel.kamatchi@otsi-usa.com>
2018-08-04 21:28:33 -0500

    Added authentication implementation.............

---
+++ src/main/java/com/recipeworld/knockmykitchen/service/UserDetailsServiceImpl.java
@@ -0,0 +1,47 @@
+package com.recipeworld.knockmykitchen.service;
+
+import com.recipeworld.knockmykitchen.models.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    public static List<User> users = new ArrayList<>();
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    static {
+        users.add(new User("shreyas", "1234"));
+        users.add(new User("sahasra", "5678"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LOGGER.info("Inside UserDetailsServiceImpl --------------- loadUserByUsername method.........." + username);
+        Optional<User> user = users.stream()
+                .filter(u -> u.getUserName().equals(username))
+                .findAny();
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found by name: " + username);
+        }
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+
+        return new org.springframework.security.core.userdetails.User(user.get().getUserName(), user.get().getPassword(), grantedAuthorities);
+    }
+}
